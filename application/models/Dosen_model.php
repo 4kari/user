e2dbe4736a1d942962d959d2008675087545b8ff
4kari <mypasshidden@gmail.com>
2021-10-06 16:54:40 +0700

    update tambah dosen

--- application/models/Dosen_model.php
+++ application/models/Dosen_model.php
@@ -12,12 +12,11 @@ class Dosen_model extends CI_Model{
         return $this->db->affected_rows();
     }
     public function createDosen($data){
-        $this->db->insert('Dosen',$data);
         $nip = $data['nip'];
         $nama = $data['nama'];
-        if($nip!="" && $nama!=""){
+        if($nip && $nama){
             // $user = json_decode($this->curl->simple_get('http://10.5.12.26/user/api/User/'),true);
-            $user = json_decode($this->curl->simple_get('http://localhost/microservice/user/api/User/',array('username' => $)),true);
+            $user = json_decode($this->curl->simple_get('http://localhost/microservice/user/api/User/',array('username' => $nip)),true);
             $cek=false;
             for ($i=0;$i<count($user);$i++){
                 if($user['data'][$i]['username']==$nip){
@@ -25,7 +24,6 @@ class Dosen_model extends CI_Model{
                 }
             }
             if($cek==false){
-                echo "user tidak ditemukan";
                 $data=[
                     'username'=>$nip,
                     'password'=>$nip,
@@ -34,8 +32,7 @@ class Dosen_model extends CI_Model{
                 // json_decode($this->curl->simple_post('http://10.5.12.26/user/api/user/',$data,array(CURLOPT_BUFFERSIZE => 10)),true);
                 json_decode($this->curl->simple_post('http://localhost/microservice/user/api/user/',$data,array(CURLOPT_BUFFERSIZE => 10)),true);
             }
-            // $dosen = json_decode($this->curl->simple_get('http://10.5.12.26/user/api/Dosen/',array('nip'=>$nip),array(CURLOPT_BUFFERSIZE => 10)),true);
-            $dosen = json_decode($this->curl->simple_get('http://localhost/microservice/user/api/Dosen/',array('nip'=>$nip),array(CURLOPT_BUFFERSIZE => 10)),true);
+            $dosen = $this->getDosen($nip);
             if ($dosen==NULL){
                 $data=[
                     'nip'=>$nip,
@@ -43,14 +40,8 @@ class Dosen_model extends CI_Model{
                     'username'=>$nip,
                     'tanggal_buat'=> date("Y-m-d",time())
                 ];
-                // json_decode($this->curl->simple_post('http://10.5.12.26/user/api/Dosen/',$data,array(CURLOPT_BUFFERSIZE => 10)),true);
-                json_decode($this->curl->simple_post('http://localhost/microservice/user/api/Dosen/',$data,array(CURLOPT_BUFFERSIZE => 10)),true);
-                echo "dosen ditambahkan";
-            }else{
-                echo "data sudah ada";
+                $this->db->insert('Dosen',$data);
             }
-        }else{
-            echo "data kosong";
         }
         return $this->db->affected_rows();
     }
