ef1d1aab8edc537b7e2c6e8390e62e1ec2f754fd
4kari <mypasshidden@gmail.com>
2021-10-08 16:37:57 +0700

    update add mhs

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -14,30 +14,38 @@ class Mahasiswa_model extends CI_Model{
     public function createMahasiswa($data){
         $nim = $data['nim'];
         $nama = $data['nama'];
-        // $user = json_decode($this->curl->simple_get('http://10.5.12.26/user/api/User/',array('username'=>$nim)),true);
-        $user = json_decode($this->curl->simple_get('http://localhost/microservice/user/api/User/',array('username'=>$nim)),true);
+        $prodi=substr($nim, 4, 3);
+        // $Cprodi = json_decode($this->curl->simple_get('http://10.5.12.26/user/api/Prodi/',array('kode_prodi'=>$prodi)),true);
+        $Cprodi = json_decode($this->curl->simple_get('http://localhost/microservice/user/api/Prodi/',array('kode_prodi'=>$prodi)),true);
+        var_dump($Cprodi);
+        if ($Cprodi){
+            echo "jalan";
+            // $user = json_decode($this->curl->simple_get('http://10.5.12.26/user/api/User/',array('username'=>$nim)),true);
+            $user = json_decode($this->curl->simple_get('http://localhost/microservice/user/api/User/',array('username'=>$nim)),true);
 
-        if(!$user){
-            $data=[
-                'username'=>$nim,
-                'password'=>$nim,
-                'level'=>"4"
-            ];
-            // json_decode($this->curl->simple_post('http://10.5.12.26/user/api/user/',$data,array(CURLOPT_BUFFERSIZE => 10)),true);
-            json_decode($this->curl->simple_post('http://localhost/microservice/user/api/user/',$data,array(CURLOPT_BUFFERSIZE => 10)),true);
-        }
-        $mhs = $this->getMahasiswa($nim);
-
-        if (!$mhs){
-            $data=[
-                'nim'=>$nim,
-                'nama'=>$nama,
-                'username'=>$nim,
-                'prodi'=> substr($nim, 4, 3),
-                'tanggal_buat'=> date("Y-m-d",time())
-            ];
-        $this->db->insert('Mahasiswa',$data);
-        return $this->db->affected_rows();
+            if(!$user){
+                $data=[
+                    'username'=>$nim,
+                    'password'=>$nim,
+                    'level'=>"4"
+                ];
+                // json_decode($this->curl->simple_post('http://10.5.12.26/user/api/user/',$data,array(CURLOPT_BUFFERSIZE => 10)),true);
+                json_decode($this->curl->simple_post('http://localhost/microservice/user/api/user/',$data,array(CURLOPT_BUFFERSIZE => 10)),true);
+            }
+            $mhs = $this->getMahasiswa($nim);
+            if (!$mhs){
+                $data=[
+                    'nim'=>$nim,
+                    'nama'=>$nama,
+                    'username'=>$nim,
+                    'prodi'=> substr($nim, 4, 3),
+                    'tanggal_buat'=> date("Y-m-d",time())
+                ];
+            $this->db->insert('Mahasiswa',$data);
+            return $this->db->affected_rows();
+            }
+        }else{
+            return null;
         }
     }
     public function updateMahasiswa($data,$nim){
