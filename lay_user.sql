7c20e6d8206d06ddde764474c6a01df2d62772c3
4kari <mypasshidden@gmail.com>
2021-11-18 20:35:50 +0700

    update db

--- lay_user.sql
+++ lay_user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 07 Nov 2021 pada 13.35
+-- Waktu pembuatan: 18 Nov 2021 pada 14.30
 -- Versi server: 10.4.19-MariaDB
 -- Versi PHP: 7.4.19
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `dosen` (
-  `nip` varchar(12) NOT NULL,
+  `nip` varchar(18) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `jenis_kelamin` int(1) DEFAULT NULL,
   `alamat` varchar(32) DEFAULT NULL,
@@ -47,7 +47,11 @@ CREATE TABLE `dosen` (
 
 INSERT INTO `dosen` (`nip`, `nama`, `jenis_kelamin`, `alamat`, `tanggal_lahir`, `no_hp`, `gambar`, `username`, `prodi`, `email`, `tanggal_buat`) VALUES
 ('170411100024', 'Moh Irsad', NULL, NULL, NULL, NULL, NULL, '170411100024', NULL, NULL, '2021-08-08'),
-('170411100042', 'ria rostiani2', 2, 'utm', '0000-00-00', '082319856686', 'ia.jpg', '170411100042', '111', 'meichan12348765@gmail.com', '0000-00-00');
+('170411100042', 'ria rostiani2', 2, 'utm', '0000-00-00', '082319856686', 'ia.jpg', '170411100042', '111', 'meichan12348765@gmail.com', '0000-00-00'),
+('197406102008121002', 'Abdullah Basuki Rahmat, S.Si, ', 1, 'kososng', '2021-11-02', '123123', '12312312.jpg', '197406102008121002', '111', '123123@gmail.com', '2021-11-18'),
+('198002232008121001', 'Aeri Rachmad, S.T., M.T.', 1, 'kososng', '2021-11-01', '123123', '12312312.jpg', '198002232008121001', '111', '123123@gmail.com', '2021-11-18'),
+('198101092006041003', 'Achmad Jauhari, S.T., M.Kom', 1, 'kososng', '2021-11-01', '123123', '12312312.jpg', '198101092006041003', '111', '123123@gmail.com', '2021-11-18'),
+('198609262014041001', 'Ach. Khozaimi, S.Kom.,M.Kom', 1, 'kososng', '2021-11-01', '123123', '12312312.jpg', '198609262014041001', '111', '123123@gmail.com', '2021-11-18');
 
 -- --------------------------------------------------------
 
@@ -178,6 +182,10 @@ INSERT INTO `user` (`username`, `password`, `level`) VALUES
 ('170411100099', '170411100099', 4),
 ('170411100119', '170411100119', 4),
 ('170411100122', '170411100122', 4),
+('197406102008121002', '197406102008121002', 3),
+('198002232008121001', '198002232008121001', 3),
+('198101092006041003', '198101092006041003', 3),
+('198609262014041001', '198609262014041001', 3),
 ('Admin', 'admin', 1),
 ('koordinator', 'koordinator', 2);
 
